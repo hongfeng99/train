f107cdcea703d463973b121fafde87f79bf67522
爱坤 <fhon0005@student.monash.edu>
2025-10-22 20:28:28 +0800

    4.15 实现一个简单的注册接口

--- member/src/main/java/com/ikun/train/member/controller/MemberController.java
+++ member/src/main/java/com/ikun/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ikun.train.member.controller;
 import com.ikun.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,9 @@ public class MemberController {
         return memberService.count();
     }
 
-
+    @PostMapping("/register")
+    public long register(String mobile){
+        return memberService.register(mobile);
+    }
 }
 
--- member/src/main/java/com/ikun/train/member/service/MemberService.java
+++ member/src/main/java/com/ikun/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.ikun.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.ikun.train.member.domain.Member;
+import com.ikun.train.member.domain.MemberExample;
 import com.ikun.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -13,5 +18,20 @@ public class MemberService {
 
     public int count(){
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile){
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> members = memberMapper.selectByExample(memberExample);
+        if(CollUtil.isNotEmpty(members)){
+            throw new RuntimeException("手机号已注册");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
