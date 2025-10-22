103573ff5285a31ebbb87381b33af859eb222fcb
爱坤 <fhon0005@student.monash.edu>
2025-10-23 03:39:44 +0800

    4.20 使用雪花算法生成数据库ID

--- member/src/main/java/com/ikun/train/member/service/MemberService.java
+++ member/src/main/java/com/ikun/train/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.ikun.train.member.service;
 import cn.hutool.core.collection.CollUtil;
 import com.ikun.train.common.exception.BusinessException;
 import com.ikun.train.common.exception.BusinessExceptionEnum;
+import com.ikun.train.common.util.SnowUtil;
 import com.ikun.train.member.domain.Member;
 import com.ikun.train.member.domain.MemberExample;
 import com.ikun.train.member.mapper.MemberMapper;
@@ -33,7 +34,7 @@ public class MemberService {
         }
 
         Member member = new Member();
-        member.setId(System.currentTimeMillis());
+        member.setId(SnowUtil.getSnowflakeNextId());
         member.setMobile(mobile);
         memberMapper.insert(member);
         return member.getId();
