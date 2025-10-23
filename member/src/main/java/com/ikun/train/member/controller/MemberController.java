5643c13e13e75a6f1670df6155d9df982dcb530f
爱坤 <fhon0005@student.monash.edu>
2025-10-24 00:57:26 +0800

    5.8 增加短信验证码登录接口，升级hutool，支持列表复制。

--- member/src/main/java/com/ikun/train/member/controller/MemberController.java
+++ member/src/main/java/com/ikun/train/member/controller/MemberController.java
@@ -1,8 +1,10 @@
 package com.ikun.train.member.controller;
 
 import com.ikun.train.common.resp.CommonResp;
+import com.ikun.train.member.req.MemberLoginReq;
 import com.ikun.train.member.req.MemberRegisterReq;
 import com.ikun.train.member.req.MemberSendCodeReq;
+import com.ikun.train.member.resp.MemberLoginResp;
 import com.ikun.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
@@ -35,10 +37,17 @@ public class MemberController {
 //        return commonResp;
         return new CommonResp<>(id);
     }
+
     @PostMapping("/send-code")
     public CommonResp<Long> sendCode(@Valid MemberSendCodeReq req){
         memberService.sendCode(req);
         return new CommonResp<>();
+    }
+
+    @PostMapping("/login")
+    public CommonResp<MemberLoginResp> login(@Valid MemberLoginReq req){
+        MemberLoginResp resp = memberService.login(req);
+        return new CommonResp<>(resp);
     }
 }
 
--- member/src/main/java/com/ikun/train/member/service/MemberService.java
+++ member/src/main/java/com/ikun/train/member/service/MemberService.java
@@ -1,14 +1,18 @@
 package com.ikun.train.member.service;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ObjectUtil;
 import com.ikun.train.common.exception.BusinessException;
 import com.ikun.train.common.exception.BusinessExceptionEnum;
 import com.ikun.train.common.util.SnowUtil;
 import com.ikun.train.member.domain.Member;
 import com.ikun.train.member.domain.MemberExample;
 import com.ikun.train.member.mapper.MemberMapper;
+import com.ikun.train.member.req.MemberLoginReq;
 import com.ikun.train.member.req.MemberRegisterReq;
 import com.ikun.train.member.req.MemberSendCodeReq;
+import com.ikun.train.member.resp.MemberLoginResp;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,10 +34,8 @@ public class MemberService {
 
     public long register(MemberRegisterReq req){
         String mobile = req.getMobile();
-        MemberExample memberExample = new MemberExample();
-        memberExample.createCriteria().andMobileEqualTo(mobile);
-        List<Member> members = memberMapper.selectByExample(memberExample);
-        if(CollUtil.isNotEmpty(members)){
+        Member memberDB = selectByMobile(mobile);
+        if(ObjectUtil.isNotNull(memberDB)){
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
 
@@ -43,14 +45,13 @@ public class MemberService {
         memberMapper.insert(member);
         return member.getId();
     }
+
     public void sendCode(MemberSendCodeReq req){
         String mobile = req.getMobile();
-        MemberExample memberExample = new MemberExample();
-        memberExample.createCriteria().andMobileEqualTo(mobile);
-        List<Member> members = memberMapper.selectByExample(memberExample);
+        Member memberDB = selectByMobile(mobile);
 
         // 如果手机号不存在，则插入一条记录
-        if(CollUtil.isEmpty(members)){
+        if(ObjectUtil.isNull(memberDB)){
             LOG.info("手机号不存在，插入一条记录");
             Member member = new Member();
             member.setId(SnowUtil.getSnowflakeNextId());
@@ -71,5 +72,33 @@ public class MemberService {
         // 对接短信通道，发送短信
         LOG.info("对接短信通道");
 
+    }
+        public MemberLoginResp login(MemberLoginReq req){
+        String mobile = req.getMobile();
+        String code = req.getCode();
+        Member memberDB= selectByMobile(mobile);
+
+        // 如果手机号不存在，则插入一条记录
+        if(ObjectUtil.isNull(memberDB)){
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_NOT_EXIST);
+        }
+
+        // 校验短信验证码
+            if(!"9999".equals(code)){
+                throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_CODE_ERROR);
+            }
+            return BeanUtil.copyProperties(memberDB,MemberLoginResp.class);
+    }
+
+    private Member selectByMobile(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> members = memberMapper.selectByExample(memberExample);
+        // 如果手机号不存在，则插入一条记录
+        if(CollUtil.isEmpty(members)){
+            return null;
+        }else{
+            return members.get(0);
+        }
     }
 }
