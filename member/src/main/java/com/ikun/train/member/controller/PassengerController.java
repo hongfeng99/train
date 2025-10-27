9c9599b2db2d5de8a25b18c07a6f73c960ea1732
爱坤 <fhon0005@student.monash.edu>
2025-10-27 13:39:56 +0800

    7.3 增加乘车人新增接口

---
+++ member/src/main/java/com/ikun/train/member/controller/PassengerController.java
@@ -0,0 +1,28 @@
+package com.ikun.train.member.controller;
+
+import com.ikun.train.common.resp.CommonResp;
+import com.ikun.train.member.req.PassengerSaveReq;
+import com.ikun.train.member.service.PassengerService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/passenger")
+public class PassengerController {
+
+    @Resource
+    private PassengerService passengerService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req){
+        passengerService.save(req);
+        return new CommonResp<>();
+    }
+
+}
+
