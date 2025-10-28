557b561390ca42983aa3b0fd0965cec87479f280
爱坤 <fhon0005@student.monash.edu>
2025-10-28 13:26:39 +0800

    7.9 封装分页请求参数

--- member/src/main/java/com/ikun/train/member/req/PassengerQueryReq.java
+++ member/src/main/java/com/ikun/train/member/req/PassengerQueryReq.java
@@ -1,6 +1,8 @@
 package com.ikun.train.member.req;
 
-public class PassengerQueryReq {
+import com.ikun.train.common.req.PageReq;
+
+public class PassengerQueryReq extends PageReq {
 
     private Long memberId;
 
--- member/src/main/java/com/ikun/train/member/service/PassengerService.java
+++ member/src/main/java/com/ikun/train/member/service/PassengerService.java
@@ -45,7 +45,7 @@ public class PassengerService {
             criteria.andMemberIdEqualTo(req.getMemberId());
         }
 
-        PageHelper.startPage(2,2);
+        PageHelper.startPage(req.getPage(),req.getSize());
         List<Passenger> list = passengerMapper.selectByExample(passengerExample);
         return BeanUtil.copyToList(list, PassengerQueryResp.class);
     }
