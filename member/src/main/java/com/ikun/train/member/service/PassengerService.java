b02cc3b9256d3fcf9b88cd9e2af2f315c214dcf4
爱坤 <fhon0005@student.monash.edu>
2025-10-28 14:30:01 +0800

    7.10 乘车人列表查询界面开发

--- member/src/main/java/com/ikun/train/member/service/PassengerService.java
+++ member/src/main/java/com/ikun/train/member/service/PassengerService.java
@@ -45,6 +45,7 @@ public class PassengerService {
 
     public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req){
         PassengerExample passengerExample = new PassengerExample();
+        passengerExample.setOrderByClause("id desc");
         PassengerExample.Criteria criteria = passengerExample.createCriteria();
         if(ObjectUtil.isNotEmpty(req.getMemberId())){
             criteria.andMemberIdEqualTo(req.getMemberId());
--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,5 +1,21 @@
 <template>
-  <a-button type="primary" @click="showModal">新增</a-button>
+
+  <!--p防止与下面元素重叠 -->
+  <p>
+    <a-space>
+      <a-button type="primary" @click="handlerQuery()" >刷新</a-button>
+      <a-button type="primary" @click="showModal">新增</a-button>
+    </a-space>
+  </p>
+
+
+  <!--乘车人员展示-->
+  <a-table :dataSource="passengers.list"
+           :columns="columns"
+           :pagination="pagination"
+           @change="handleTableChange"
+           :loading="loading"/>
+  <!-- 新增弹窗 -->
   <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk" ok-text="确认" cancel-text="取消">
     <a-form
         :model="passenger"
@@ -30,12 +46,14 @@
 </template>
 
 <script>
-import {defineComponent, ref, reactive} from 'vue';
+//reactive数组重新赋值会失去响应式特性
+import {defineComponent, ref, reactive, onMounted} from 'vue';
 import axios from "axios";
 import {notification} from "ant-design-vue";
 
 export default defineComponent({
   name: "passenger-view",
+
   setup() {
 
     const passenger = reactive({
@@ -47,12 +65,37 @@ export default defineComponent({
       createTime: undefined,
       updateTime: undefined,
     });
-    const onFinish = values => {
-      console.log('Success:', values);
-    };
-    const onFinishFailed = errorInfo => {
-      console.log('Failed:', errorInfo);
-    };
+
+    const pagination = reactive({
+      total: 0,
+      current: 1,
+      pageSize: 6,
+    })
+
+    const loading = ref(false)
+
+    //声明ref可以直接赋值  reactive直接复制响应式失效，变成普通变量，会发生刷新没数据问题
+    let passengers = ref({
+      list:[]
+    });
+
+    const columns = [
+      {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '身份证',
+        dataIndex: 'idCard',
+        key: 'idCard',
+      },
+      {
+        title: '类型',
+        dataIndex: 'type',
+        key: 'type',
+      },
+    ]
 
     const visible = ref(false);
     const showModal = () => {
@@ -65,19 +108,78 @@ export default defineComponent({
         if(data.success) {
           notification.success({description:"乘车人基本信息保存成功"})
           visible.value = false;
+          //保存成功后进行刷新
+          handlerQuery({
+            page: pagination.current,
+            size: pagination.pageSize
+          })
         }else {
           notification.error({description: data.message})
         }
 
       })
     };
+    const handlerQuery = param => {
+      //刷新默认不传值，向后端发送一次请求，并把页面置为1
+      if(!param) {
+
+        param = {
+          page: 1,
+          size : pagination.pageSize
+        }
+      }
+      //限制刷新次数，通过加载loading
+      loading.value = true
+      axios.get("/member/passenger/query-list",{
+        params:{
+          page: param.page,
+          size: param.size
+        }
+      }).then((response) => {
+        //无论成功还是失败 不加载动画
+        loading.value = false
+        let data = response.data;
+        if(data.success) {
+          passengers.value.list = data.content.list;
+
+          //设置分页控件的值   页码切换问题
+          pagination.current = param.page;
+          pagination.total = data.content.total;
+          // 使用reactive只能push
+          // passengers.push(...data.content.list)
+        }else{
+          notification.error({description: data.message});
+        }
+      })
+    }
+
+    //监听页码事件
+    const handleTableChange = (pagination) =>{
+      console.log("分页参数"+pagination)
+      handlerQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      })
+    }
+
+    onMounted(() => {
+      handlerQuery({
+        page: 1,
+        size: pagination.pageSize
+      })
+    })
+
     return {
       visible,
       showModal,
       handleOk,
       passenger,
-      onFinish,
-      onFinishFailed,
+      passengers,
+      columns,
+      handlerQuery,
+      pagination,
+      handleTableChange,
+      loading
     };
   },
 });
