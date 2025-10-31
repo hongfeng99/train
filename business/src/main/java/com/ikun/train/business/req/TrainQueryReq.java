0b25a5ae6da785c7f54e972b0c1761bf3546a18e
爱坤 <fhon0005@student.monash.edu>
2025-10-31 12:40:38 +0800

    9.7 增加火车表，生成持久层代码及前后端代码

---
+++ business/src/main/java/com/ikun/train/business/req/TrainQueryReq.java
@@ -0,0 +1,12 @@
+package com.ikun.train.business.req;
+
+import com.ikun.train.common.req.PageReq;
+
+public class TrainQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainQueryReq{" +
+                "} " + super.toString();
+    }
+}
