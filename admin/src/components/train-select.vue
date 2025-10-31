e629aab5f097a713d173c774970ba0f6c183f74a
爱坤 <fhon0005@student.monash.edu>
2025-10-31 16:29:53 +0800

    9.13 制作车次下拉框组件

---
+++ admin/src/components/train-select.vue
@@ -0,0 +1,85 @@
+<template>
+  <a-select v-model:value="trainCode" show-search
+            :filter-option="filterTrainCodeOption" placeholder="请选择车次"
+            @change="onChange">
+
+    <a-select-option v-for="item in trains" :key="item.code"
+                     :value="item.code" :label="item.code + item.start + item.end">
+      {{item.code}} | {{item.start}} ~ {{item.end}}
+    </a-select-option>
+
+  </a-select>
+</template>
+
+<script>
+import {defineComponent, onMounted, ref, watch} from 'vue';
+import axios from "axios";
+import {notification} from "ant-design-vue";
+
+export default defineComponent({
+  name: "train-select-view",
+  props:["modelValue"],
+  emits:['update:modelValue','change'],
+  setup(props, {emit}) {
+    const trainCode = ref();
+    const trains = ref([]);
+
+
+    //利用watch，动态获取父组件的值，如果放在onMounted或其他方法只会执行一次
+    watch(() => props.modelValue, ()=>{
+          console.log("props.modelValue",props.modelValue)
+          trainCode.value = props.modelValue;
+    },{immediate: true});
+
+    /**
+     * 查询所有的车次，用于车次下拉框
+     */
+    const queryAllTrain = () => {
+      axios.get("/business/admin/train/query-all").then((response) => {
+        let data = response.data;
+        if (data.success) {
+          trains.value = data.content;
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+
+    /**
+     * 车位下拉框筛选
+     */
+    const filterTrainCodeOption = (input, option) => {
+      console.log(input,option);
+      return option.label.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+    }
+
+    /**
+     * 将当前组件的值响应给父组件
+     * @param value
+     */
+    const onChange = (value) => {
+      emit('update:modelValue', value);
+      let train = trains.value.filter(item => item.code === value)[0];
+      if (Tool.isEmpty(train)) {
+        train = {};
+      }
+      emit('change', train);
+    };
+
+    onMounted(() => {
+      queryAllTrain();
+    })
+    return {
+      trains,
+      filterTrainCodeOption,
+      trainCode,
+      onChange
+    };
+  },
+});
+</script>
+
+<style>
+
+</style>
