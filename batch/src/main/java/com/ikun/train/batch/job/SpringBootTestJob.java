1be5112352b5cebc5a4a34830a5352de594d0c3d
爱坤 <fhon0005@student.monash.edu>
2025-11-01 15:21:33 +0800

    10.6 禁止任务并发执行

--- batch/src/main/java/com/ikun/train/batch/job/SpringBootTestJob.java
+++ batch/src/main/java/com/ikun/train/batch/job/SpringBootTestJob.java
@@ -1,23 +1,23 @@
-package com.ikun.train.batch.job;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-
-/*
-    适合单体应用，不适合集群
-    不能实时更改定时任务和策略
- */
-@Component
-@EnableScheduling
-public class SpringBootTestJob {
-
-    @Scheduled(cron = "0/5 * * * * *")
-    private void test(){
-        // 增加分布式锁
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println(now);
-    }
-}
+//package com.ikun.train.batch.job;
+//
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.LocalDateTime;
+//
+///*
+//    适合单体应用，不适合集群
+//    不能实时更改定时任务和策略
+// */
+//@Component
+//@EnableScheduling
+//public class SpringBootTestJob {
+//
+//    @Scheduled(cron = "0/5 * * * * *")
+//    private void test(){
+//        // 增加分布式锁
+//        LocalDateTime now = LocalDateTime.now();
+//        System.out.println(now);
+//    }
+//}
--- batch/src/main/java/com/ikun/train/batch/job/TestJob.java
+++ batch/src/main/java/com/ikun/train/batch/job/TestJob.java
@@ -1,15 +1,24 @@
 package com.ikun.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.context.annotation.Configuration;
 
+
+@DisallowConcurrentExecution
 @Configuration
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob TEST");
+        System.out.println("TestJob TEST开始");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJob TEST结束");
     }
 }
