334c7d9ac265781603fca583b095ac6e69755be3
爱坤 <fhon0005@student.monash.edu>
2025-11-01 10:25:14 +0800

    9.18 保存车站之前，先校验唯一键是否存在

--- business/src/main/java/com/ikun/train/business/service/StationService.java
+++ business/src/main/java/com/ikun/train/business/service/StationService.java
@@ -1,18 +1,21 @@
 package com.ikun.train.business.service;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.ikun.train.common.resp.PageResp;
-import com.ikun.train.common.util.SnowUtil;
 import com.ikun.train.business.domain.Station;
 import com.ikun.train.business.domain.StationExample;
 import com.ikun.train.business.mapper.StationMapper;
 import com.ikun.train.business.req.StationQueryReq;
 import com.ikun.train.business.req.StationSaveReq;
 import com.ikun.train.business.resp.StationQueryResp;
+import com.ikun.train.common.exception.BusinessException;
+import com.ikun.train.common.exception.BusinessExceptionEnum;
+import com.ikun.train.common.resp.PageResp;
+import com.ikun.train.common.util.SnowUtil;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,6 +35,13 @@ public class StationService {
         Station station = BeanUtil.copyProperties(req, Station.class);
 
         if (ObjectUtil.isNull(req.getId())) {
+
+            // 保存之前，先校验唯一键是否存在
+            Station stationDB = selectByUnique(req.getName());
+            if (ObjectUtil.isNotEmpty(stationDB)) {
+                throw new BusinessException(BusinessExceptionEnum.BUSINESS_STATION_NAME_UNIQUE_ERROR);
+            }
+
             station.setId(SnowUtil.getSnowflakeNextId());
             station.setCreateTime(now);
             station.setUpdateTime(now);
@@ -42,6 +52,17 @@ public class StationService {
         }
     }
 
+
+    private Station selectByUnique(String name) {
+        StationExample stationExample = new StationExample();
+        stationExample.createCriteria().andNameEqualTo(name);
+        List<Station> list = stationMapper.selectByExample(stationExample);
+        if (CollUtil.isNotEmpty(list)) {
+            return list.get(0);
+        } else {
+            return null;
+        }
+    }
     public PageResp<StationQueryResp> queryList(StationQueryReq req) {
         StationExample stationExample = new StationExample();
         stationExample.setOrderByClause("id desc");
