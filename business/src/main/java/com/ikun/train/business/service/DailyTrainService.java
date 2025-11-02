5481ffd0fee2d2b47e5bd61150e97c679cd289cb
爱坤 <fhon0005@student.monash.edu>
2025-11-02 08:52:24 +0800

    11.3 为每日车次页面增加日期和车次查询条件，注意：GET请求的日期是拼接在URL里的，需要用spring自带的    @DateTimeFormat(pattern = "yyyy-MM-dd") ，后端才能接收到参数

--- business/src/main/java/com/ikun/train/business/service/DailyTrainService.java
+++ business/src/main/java/com/ikun/train/business/service/DailyTrainService.java
@@ -22,42 +22,47 @@ import java.util.List;
 
 @Service
 public class DailyTrainService {
-private static final Logger LOG = LoggerFactory.getLogger(DailyTrainService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainService.class);
 
-@Resource
-private DailyTrainMapper dailyTrainMapper;
+    @Resource
+    private DailyTrainMapper dailyTrainMapper;
 
-public void save(DailyTrainSaveReq req){
-DateTime now = DateTime.now();
-DailyTrain dailyTrain = BeanUtil.copyProperties(req, DailyTrain.class);
+    public void save(DailyTrainSaveReq req) {
+        DateTime now = DateTime.now();
+        DailyTrain dailyTrain = BeanUtil.copyProperties(req, DailyTrain.class);
 
-if(ObjectUtil.isNull(req.getId())){
-dailyTrain.setId(SnowUtil.getSnowflakeNextId());
-dailyTrain.setCreateTime(now);
-dailyTrain.setUpdateTime(now);
-dailyTrainMapper.insert(dailyTrain);
-}else{
-dailyTrain.setUpdateTime(now);
-dailyTrainMapper.updateByPrimaryKey(dailyTrain);
-}
-}
+        if (ObjectUtil.isNull(req.getId())) {
+            dailyTrain.setId(SnowUtil.getSnowflakeNextId());
+            dailyTrain.setCreateTime(now);
+            dailyTrain.setUpdateTime(now);
+            dailyTrainMapper.insert(dailyTrain);
+        } else {
+            dailyTrain.setUpdateTime(now);
+            dailyTrainMapper.updateByPrimaryKey(dailyTrain);
+        }
+    }
 
-public PageResp<DailyTrainQueryResp> queryList(DailyTrainQueryReq req){
-    DailyTrainExample dailyTrainExample = new DailyTrainExample();
-    dailyTrainExample.setOrderByClause("id desc");
-    DailyTrainExample.Criteria criteria = dailyTrainExample.createCriteria();
+    public PageResp<DailyTrainQueryResp> queryList(DailyTrainQueryReq req) {
+        DailyTrainExample dailyTrainExample = new DailyTrainExample();
+        dailyTrainExample.setOrderByClause("date desc, code asc");
+        DailyTrainExample.Criteria criteria = dailyTrainExample.createCriteria();
+        if (ObjectUtil.isNotNull(req.getDate())) {
+            criteria.andDateEqualTo(req.getDate());
+        }
+        if (ObjectUtil.isNotEmpty(req.getCode())) {
+            criteria.andCodeEqualTo(req.getCode());
+        }
 
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<DailyTrain> list = dailyTrainMapper.selectByExample(dailyTrainExample);
 
-    LOG.info("查询页码：{}", req.getPage());
-    LOG.info("每页条数：{}", req.getSize());
-    PageHelper.startPage(req.getPage(),req.getSize());
-    List<DailyTrain> list = dailyTrainMapper.selectByExample(dailyTrainExample);
+        PageInfo<DailyTrain> pageInfo = new PageInfo<>(list);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
-    PageInfo<DailyTrain> pageInfo = new PageInfo<>(list);
-    LOG.info("总行数：{}", pageInfo.getTotal());
-    LOG.info("总页数：{}", pageInfo.getPages());
-
-    List<DailyTrainQueryResp> list1 = BeanUtil.copyToList(list, DailyTrainQueryResp.class);
+        List<DailyTrainQueryResp> list1 = BeanUtil.copyToList(list, DailyTrainQueryResp.class);
         PageResp<DailyTrainQueryResp> pageResp = new PageResp<>();
         pageResp.setTotal(pageInfo.getTotal());
         pageResp.setList(list1);
