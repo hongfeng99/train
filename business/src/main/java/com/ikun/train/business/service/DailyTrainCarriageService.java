a7effcbf18005cf54f539be14bc060edae8257c9
爱坤 <fhon0005@student.monash.edu>
2025-11-02 10:16:57 +0800

    11.5 将column改成col，重新生成代码，增加日期和车次查询条件

--- business/src/main/java/com/ikun/train/business/service/DailyTrainCarriageService.java
+++ business/src/main/java/com/ikun/train/business/service/DailyTrainCarriageService.java
@@ -2,17 +2,19 @@ package com.ikun.train.business.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.ikun.train.common.resp.PageResp;
-import com.ikun.train.common.util.SnowUtil;
 import com.ikun.train.business.domain.DailyTrainCarriage;
 import com.ikun.train.business.domain.DailyTrainCarriageExample;
+import com.ikun.train.business.enums.SeatColEnum;
 import com.ikun.train.business.mapper.DailyTrainCarriageMapper;
 import com.ikun.train.business.req.DailyTrainCarriageQueryReq;
 import com.ikun.train.business.req.DailyTrainCarriageSaveReq;
 import com.ikun.train.business.resp.DailyTrainCarriageQueryResp;
+import com.ikun.train.common.resp.PageResp;
+import com.ikun.train.common.util.SnowUtil;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,42 +24,53 @@ import java.util.List;
 
 @Service
 public class DailyTrainCarriageService {
-private static final Logger LOG = LoggerFactory.getLogger(DailyTrainCarriageService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainCarriageService.class);
 
-@Resource
-private DailyTrainCarriageMapper dailyTrainCarriageMapper;
+    @Resource
+    private DailyTrainCarriageMapper dailyTrainCarriageMapper;
 
-public void save(DailyTrainCarriageSaveReq req){
-DateTime now = DateTime.now();
-DailyTrainCarriage dailyTrainCarriage = BeanUtil.copyProperties(req, DailyTrainCarriage.class);
+    public void save(DailyTrainCarriageSaveReq req) {
+        DateTime now = DateTime.now();
+        // 自动计算出列数和总座位数
+        List<SeatColEnum> seatColEnums = SeatColEnum.getColsByType(req.getSeatType());
+        req.setColCount(seatColEnums.size());
+        req.setSeatCount(req.getColCount() * req.getRowCount());
 
-if(ObjectUtil.isNull(req.getId())){
-dailyTrainCarriage.setId(SnowUtil.getSnowflakeNextId());
-dailyTrainCarriage.setCreateTime(now);
-dailyTrainCarriage.setUpdateTime(now);
-dailyTrainCarriageMapper.insert(dailyTrainCarriage);
-}else{
-dailyTrainCarriage.setUpdateTime(now);
-dailyTrainCarriageMapper.updateByPrimaryKey(dailyTrainCarriage);
-}
-}
+        DailyTrainCarriage dailyTrainCarriage = BeanUtil.copyProperties(req, DailyTrainCarriage.class);
 
-public PageResp<DailyTrainCarriageQueryResp> queryList(DailyTrainCarriageQueryReq req){
-    DailyTrainCarriageExample dailyTrainCarriageExample = new DailyTrainCarriageExample();
-    dailyTrainCarriageExample.setOrderByClause("id desc");
-    DailyTrainCarriageExample.Criteria criteria = dailyTrainCarriageExample.createCriteria();
+        if (ObjectUtil.isNull(req.getId())) {
+            dailyTrainCarriage.setId(SnowUtil.getSnowflakeNextId());
+            dailyTrainCarriage.setCreateTime(now);
+            dailyTrainCarriage.setUpdateTime(now);
+            dailyTrainCarriageMapper.insert(dailyTrainCarriage);
+        } else {
+            dailyTrainCarriage.setUpdateTime(now);
+            dailyTrainCarriageMapper.updateByPrimaryKey(dailyTrainCarriage);
+        }
+    }
+
+    public PageResp<DailyTrainCarriageQueryResp> queryList(DailyTrainCarriageQueryReq req) {
+        DailyTrainCarriageExample dailyTrainCarriageExample = new DailyTrainCarriageExample();
+        dailyTrainCarriageExample.setOrderByClause("date desc, train_code asc, `index` asc");
+        DailyTrainCarriageExample.Criteria criteria = dailyTrainCarriageExample.createCriteria();
+        if (ObjUtil.isNotNull(req.getDate())) {
+            criteria.andDateEqualTo(req.getDate());
+        }
+        if (ObjUtil.isNotEmpty(req.getTrainCode())) {
+            criteria.andTrainCodeEqualTo(req.getTrainCode());
+        }
 
 
-    LOG.info("查询页码：{}", req.getPage());
-    LOG.info("每页条数：{}", req.getSize());
-    PageHelper.startPage(req.getPage(),req.getSize());
-    List<DailyTrainCarriage> list = dailyTrainCarriageMapper.selectByExample(dailyTrainCarriageExample);
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<DailyTrainCarriage> list = dailyTrainCarriageMapper.selectByExample(dailyTrainCarriageExample);
 
-    PageInfo<DailyTrainCarriage> pageInfo = new PageInfo<>(list);
-    LOG.info("总行数：{}", pageInfo.getTotal());
-    LOG.info("总页数：{}", pageInfo.getPages());
+        PageInfo<DailyTrainCarriage> pageInfo = new PageInfo<>(list);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
-    List<DailyTrainCarriageQueryResp> list1 = BeanUtil.copyToList(list, DailyTrainCarriageQueryResp.class);
+        List<DailyTrainCarriageQueryResp> list1 = BeanUtil.copyToList(list, DailyTrainCarriageQueryResp.class);
         PageResp<DailyTrainCarriageQueryResp> pageResp = new PageResp<>();
         pageResp.setTotal(pageInfo.getTotal());
         pageResp.setList(list1);
