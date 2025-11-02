717b185c20abd0bb794a9a12ae85b1d833e02564
爱坤 <fhon0005@student.monash.edu>
2025-11-02 09:18:43 +0800

    11.4 快速生成每日车站数据管理页面

---
+++ business/src/main/java/com/ikun/train/business/service/DailyTrainStationService.java
@@ -0,0 +1,70 @@
+package com.ikun.train.business.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.ikun.train.common.resp.PageResp;
+import com.ikun.train.common.util.SnowUtil;
+import com.ikun.train.business.domain.DailyTrainStation;
+import com.ikun.train.business.domain.DailyTrainStationExample;
+import com.ikun.train.business.mapper.DailyTrainStationMapper;
+import com.ikun.train.business.req.DailyTrainStationQueryReq;
+import com.ikun.train.business.req.DailyTrainStationSaveReq;
+import com.ikun.train.business.resp.DailyTrainStationQueryResp;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DailyTrainStationService {
+private static final Logger LOG = LoggerFactory.getLogger(DailyTrainStationService.class);
+
+@Resource
+private DailyTrainStationMapper dailyTrainStationMapper;
+
+public void save(DailyTrainStationSaveReq req){
+DateTime now = DateTime.now();
+DailyTrainStation dailyTrainStation = BeanUtil.copyProperties(req, DailyTrainStation.class);
+
+if(ObjectUtil.isNull(req.getId())){
+dailyTrainStation.setId(SnowUtil.getSnowflakeNextId());
+dailyTrainStation.setCreateTime(now);
+dailyTrainStation.setUpdateTime(now);
+dailyTrainStationMapper.insert(dailyTrainStation);
+}else{
+dailyTrainStation.setUpdateTime(now);
+dailyTrainStationMapper.updateByPrimaryKey(dailyTrainStation);
+}
+}
+
+public PageResp<DailyTrainStationQueryResp> queryList(DailyTrainStationQueryReq req){
+    DailyTrainStationExample dailyTrainStationExample = new DailyTrainStationExample();
+    dailyTrainStationExample.setOrderByClause("id desc");
+    DailyTrainStationExample.Criteria criteria = dailyTrainStationExample.createCriteria();
+
+
+    LOG.info("查询页码：{}", req.getPage());
+    LOG.info("每页条数：{}", req.getSize());
+    PageHelper.startPage(req.getPage(),req.getSize());
+    List<DailyTrainStation> list = dailyTrainStationMapper.selectByExample(dailyTrainStationExample);
+
+    PageInfo<DailyTrainStation> pageInfo = new PageInfo<>(list);
+    LOG.info("总行数：{}", pageInfo.getTotal());
+    LOG.info("总页数：{}", pageInfo.getPages());
+
+    List<DailyTrainStationQueryResp> list1 = BeanUtil.copyToList(list, DailyTrainStationQueryResp.class);
+        PageResp<DailyTrainStationQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list1);
+        return pageResp;
+    }
+
+    public void delete(Long id) {
+        dailyTrainStationMapper.deleteByPrimaryKey(id);
+    }
+}
