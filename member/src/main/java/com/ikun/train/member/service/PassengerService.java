3bb3f6842e58074a9af98ff92c890e1b48ebc429
爱坤 <fhon0005@student.monash.edu>
2025-11-05 16:16:02 +0800

    12.8 订票页面，查询我的所有的乘客（可以在新增乘客的时候，增加一个校验：超过50,就无法再新增）

--- member/src/main/java/com/ikun/train/member/service/PassengerService.java
+++ member/src/main/java/com/ikun/train/member/service/PassengerService.java
@@ -23,43 +23,43 @@ import java.util.List;
 
 @Service
 public class PassengerService {
-private static final Logger LOG = LoggerFactory.getLogger(PassengerService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(PassengerService.class);
 
-@Resource
-private PassengerMapper passengerMapper;
+    @Resource
+    private PassengerMapper passengerMapper;
 
-public void save(PassengerSaveReq req){
-DateTime now = DateTime.now();
-Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+    public void save(PassengerSaveReq req) {
+        DateTime now = DateTime.now();
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
 
-if(ObjectUtil.isNull(req.getId())){
-    passenger.setMemberId(LoginMemberContext.getId());
-    passenger.setId(SnowUtil.getSnowflakeNextId());
-passenger.setCreateTime(now);
-passenger.setUpdateTime(now);
-passengerMapper.insert(passenger);
-}else{
-passenger.setUpdateTime(now);
-passengerMapper.updateByPrimaryKey(passenger);
-}
-}
+        if (ObjectUtil.isNull(req.getId())) {
+            passenger.setMemberId(LoginMemberContext.getId());
+            passenger.setId(SnowUtil.getSnowflakeNextId());
+            passenger.setCreateTime(now);
+            passenger.setUpdateTime(now);
+            passengerMapper.insert(passenger);
+        } else {
+            passenger.setUpdateTime(now);
+            passengerMapper.updateByPrimaryKey(passenger);
+        }
+    }
 
-public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req){
-    PassengerExample passengerExample = new PassengerExample();
-    passengerExample.setOrderByClause("id desc");
-    PassengerExample.Criteria criteria = passengerExample.createCriteria();
+    public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req) {
+        PassengerExample passengerExample = new PassengerExample();
+        passengerExample.setOrderByClause("id desc");
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
 
 
-    LOG.info("查询页码：{}", req.getPage());
-    LOG.info("每页条数：{}", req.getSize());
-    PageHelper.startPage(req.getPage(),req.getSize());
-    List<Passenger> list = passengerMapper.selectByExample(passengerExample);
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Passenger> list = passengerMapper.selectByExample(passengerExample);
 
-    PageInfo<Passenger> pageInfo = new PageInfo<>(list);
-    LOG.info("总行数：{}", pageInfo.getTotal());
-    LOG.info("总页数：{}", pageInfo.getPages());
+        PageInfo<Passenger> pageInfo = new PageInfo<>(list);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
-    List<PassengerQueryResp> list1 = BeanUtil.copyToList(list, PassengerQueryResp.class);
+        List<PassengerQueryResp> list1 = BeanUtil.copyToList(list, PassengerQueryResp.class);
         PageResp<PassengerQueryResp> pageResp = new PageResp<>();
         pageResp.setTotal(pageInfo.getTotal());
         pageResp.setList(list1);
@@ -68,5 +68,17 @@ public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req){
 
     public void delete(Long id) {
         passengerMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 查询我的所有乘客
+     */
+    public List<PassengerQueryResp> queryMine() {
+        PassengerExample passengerExample = new PassengerExample();
+        passengerExample.setOrderByClause("name asc");
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        criteria.andMemberIdEqualTo(LoginMemberContext.getId());
+        List<Passenger> list = passengerMapper.selectByExample(passengerExample);
+        return BeanUtil.copyToList(list, PassengerQueryResp.class);
     }
 }
--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -16,17 +16,20 @@
       </span>
     </div>
   </div>
+  <a-divider></a-divider>
+  <div>{{passengers}}</div>
 </template>
 
 <script>
-import {defineComponent} from "vue";
+import {defineComponent, onMounted, ref} from "vue";
+import axios from "axios";
 
 export default defineComponent({
   name: "order-view",
   setup(){
+    const passengers = ref({});
     const dailyTrainTicket = SessionStorage.get(SESSION_ORDER) || {}
     console.log("下单车票信息", dailyTrainTicket);
-
     const SEAT_TYPE = window.SEAT_TYPE;
     console.log(SEAT_TYPE)
     // 本车次提供的座位类型seatTypes，含票价，余票等信息，例：
@@ -57,9 +60,26 @@ export default defineComponent({
     console.log("本车次提供的座位：", seatTypes)
 
 
+    const handleQueryPassenger = () => {
+      axios.get("/member/passenger/query-mine").then((response) => {
+        let data = response.data;
+        if (data.success) {
+          passengers.value = data.content;
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+    onMounted(() =>{
+      handleQueryPassenger()
+    })
+
     return{
       dailyTrainTicket,
-      seatTypes
+      seatTypes,
+      handleQueryPassenger,
+      passengers
     }
   }
 })
