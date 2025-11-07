c9ca3a100d30f3093d21eacc1e16c95a6ded75cd
爱坤 <fhon0005@student.monash.edu>
2025-11-07 08:09:33 +0800

    12.10 余票数量小于20时，不允许选座

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -84,12 +84,10 @@
         </a-col>
       </a-row>
       <br/>
-      选座对象chooseSeatObj: {{chooseSeatObj}}
-      <br/>
       <div v-if="chooseSeatType === 0" style="color: red;">
         您购买的车票不支持选座
-        <div>12306规则：只有全部是一等座或全部是二等座才支持选座</div>
-        <div>12306规则：余票数量小于20时，不允许选座</div>
+        <div>只有全部是一等座或全部是二等座才支持选座</div>
+        <div>余票数量小于20时，不允许选座</div>
       </div>
       <div v-else style="text-align: center">
         <a-switch class="choose-seat-item" v-for="item in SEAT_COL_ARRAY" :key="item.code"
@@ -271,6 +269,23 @@ export default defineComponent({
         } else {
           console.log("不是一等座或二等座，不支持选座");
           chooseSeatType.value = 0;
+        }
+
+
+        // 余票小于20张时，不允许选座，否则选座成功率不高，影响出票
+        if (chooseSeatType.value !== 0) {
+          for (let i = 0; i < seatTypes.length; i++) {
+            let seatType = seatTypes[i];
+            // 找到同类型座位
+            if (ticketSeatTypeCodesSet[0] === seatType.code) {
+              // 判断余票，小于20张就不支持选座
+              if (seatType.count < 20) {
+                console.log("余票小于20张就不支持选座")
+                chooseSeatType.value = 0;
+                break;
+              }
+            }
+          }
         }
 
       }
