423e4fa6cf49203632ef8cbf78b94bd51036d06b
爱坤 <fhon0005@student.monash.edu>
2025-11-10 13:44:49 +0800

    12.22 增加一个feign接口，保存车票信息

---
+++ member/src/main/java/com/ikun/train/member/controller/feign/FeignTicketController.java
@@ -0,0 +1,26 @@
+package com.ikun.train.member.controller.feign;
+
+
+import com.ikun.train.common.req.MemberTicketReq;
+import com.ikun.train.common.resp.CommonResp;
+import com.ikun.train.member.service.TicketService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/feign/ticket")
+public class FeignTicketController {
+
+
+    @Autowired
+    private TicketService ticketService;
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody MemberTicketReq req) throws Exception {
+        ticketService.save(req);
+        return new CommonResp<>();
+    }
+}
