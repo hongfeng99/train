d4ce277e82bf959e326d5c4c386ef03028990fdc
爱坤 <fhon0005@student.monash.edu>
2025-11-15 16:23:32 +0800

    18.5 先获取令牌锁，再校验令牌余量，防止机器人抢票

--- business/src/main/java/com/ikun/train/business/service/SkTokenService.java
+++ business/src/main/java/com/ikun/train/business/service/SkTokenService.java
@@ -18,10 +18,12 @@ import com.ikun.train.common.util.SnowUtil;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SkTokenService {
@@ -38,6 +40,9 @@ public class SkTokenService {
 
     @Resource
     private SkTokenMapperCust skTokenMapperCust;
+
+    @Resource
+    private RedisTemplate redisTemplate;
     public void genDaily(Date date, String trainCode) {
         LOG.info("删除日期【{}】车次【{}】的令牌记录", DateUtil.formatDate(date), trainCode);
         SkTokenExample skTokenExample = new SkTokenExample();
@@ -109,6 +114,16 @@ public class SkTokenService {
 
     public boolean validSkToken(Date date, String trainCode, Long memberId) {
         LOG.info("会员【{}】获取日期【{}】车次【{}】的令牌开始", memberId, DateUtil.formatDate(date), trainCode);
+
+        // 先获取令牌锁，再校验令牌余量，防止机器人抢票，lockKey就是令牌，用来表示【谁能做什么】的一个凭证
+        String lockKey = DateUtil.formatDate(date) + "-" + trainCode + "-" + memberId;
+        Boolean setIfAbsent = redisTemplate.opsForValue().setIfAbsent(lockKey, lockKey, 5, TimeUnit.SECONDS);
+        if (Boolean.TRUE.equals(setIfAbsent)) {
+            LOG.info("恭喜，抢到令牌锁了！lockKey: {}", lockKey);
+        } else {
+            LOG.info("很遗憾，没抢到令牌锁！lockKey: {}", lockKey);
+            return false;
+        }
 
         // 令牌约等于库存，令牌没有了，就不再卖票，不需要再进入购票主流程去判断库存，判断令牌肯定比判断库存快
         int updateCount = skTokenMapperCust.decrease(date, trainCode);
