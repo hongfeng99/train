f8b5d9d54edaa7d38e53041084fce88f4fb4160a
爱坤 <fhon0005@student.monash.edu>
2025-11-15 16:37:55 +0800

    18.6 修复bug：计算车站数量有误；修改抢不到令牌锁的提示

--- business/src/main/java/com/ikun/train/business/service/SkTokenService.java
+++ business/src/main/java/com/ikun/train/business/service/SkTokenService.java
@@ -61,11 +61,11 @@ public class SkTokenService {
         int seatCount = dailyTrainSeatService.countSeat(date, trainCode);
         LOG.info("车次【{}】座位数: {}", trainCode, seatCount);
 
-        long stationCount = dailyTrainStationService.countByTrainCode(trainCode);
+        long stationCount = dailyTrainStationService.countByTrainCode(date,trainCode);
         LOG.info("车次【{}】到站数: {}", trainCode, stationCount);
 
-        // 比例系数3/4需要根据实际卖票比例来定，一趟火车最多可以卖（seatCount * stationCount）张火车票
-        int count = (int) (seatCount * stationCount * 3/4);
+        // 比例系数需要根据实际卖票比例来定，一趟火车最多可以卖（seatCount * stationCount）张火车票
+        int count = (int) (seatCount * stationCount * 1);
         LOG.info("车次【{}】初始生成令牌数: {}", trainCode, count);
         skToken.setCount(count);
 
